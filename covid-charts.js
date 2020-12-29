1c2eef1850a0373bd03b0e4d96c32ff336340853
nairvishnumail <76607707+nairvishnumail@users.noreply.github.com>
2020-12-28 20:17:32 -0800

    Add files via upload

---
+++ covid-charts.js
@@ -0,0 +1,117 @@
+	let selectedCounty = "Alameda";
+	
+	function getPastDate(days) {
+	    currentDate = new Date();
+	    currentDate.setDate(currentDate.getDate() - days);
+	    const offset = currentDate.getTimezoneOffset();
+	    currentDate = new Date(currentDate.getTime() - (offset*60*1000));
+	    return currentDate.toISOString().split('T')[0];
+	}
+	
+	
+	function getChartData() {
+	    var xmlhttp = new XMLHttpRequest();
+	    var query = "https://data.ca.gov/api/3/action/datastore_search_sql?sql=SELECT * from \"926fd08f-cc91-4828-af38-bd45de97f8c3\" WHERE \"county\" LIKE '" + selectedCounty + "' AND \"date\" >='" + getPastDate(25) + "'"
+
+	    xmlhttp.open("GET", query, false);
+	    xmlhttp.send();
+	    return JSON.parse(xmlhttp.responseText);
+	}
+	
+	function drawChart() {
+	    var myObj = getChartData();
+	    var jsonData = myObj.result.records;
+	    var caseChartData = [];
+	    var deathChartData = [];
+	    var lineCaseData = [];
+	    var lineDeathData = [];
+	    var tableData = [];
+	
+	    var tableDay = getPastDate(1);
+	    	
+	    if (jsonData.length > 0) {
+		var tableColHead = ['Total Confirmed Cases', 'Total Deaths', 'New Confirmed Cases', 'New Deaths'];
+		tableData.push(tableColHead);
+		    
+		var caseColHead = ['Date', 'New Confirmed'];
+		caseChartData.push(caseColHead); 
+		 
+		var deathColHead = ['Date', 'New Deaths'];
+		deathChartData.push(deathColHead);
+		    
+		var lineCaseColHead = ['Date', 'Total Confirmed'];
+   		lineCaseData.push(lineCaseColHead);
+		   
+		var lineDeathColHead = ['Date', 'Total Deaths'];
+   		lineDeathData.push(lineDeathColHead);
+		    		    
+		jsonData.forEach(function(row) {
+		    var rowDate = row['date'].split('T')[0];
+		    var newConfirmed = parseInt(row['newcountconfirmed']);
+		    var newDeaths = parseInt(row['newcountdeaths']);
+		    var totalConfirmed = parseInt(row['totalcountconfirmed']);
+		    var totalDeaths = parseInt(row['totalcountdeaths']);
+		   
+		    var caseChartRow = []; 
+		    caseChartRow.push(rowDate);
+		    caseChartRow.push(newConfirmed<0?0:newConfirmed);
+		    caseChartData.push(caseChartRow);
+		    
+		    var deathChartRow = [];
+		    deathChartRow.push(rowDate);
+		    deathChartRow.push(newDeaths<0?0:newDeaths);
+		    deathChartData.push(deathChartRow);
+			
+		    var lineCaseRow =[];
+       		    lineCaseRow.push(rowDate);
+       		    lineCaseRow.push(totalConfirmed<0?0:totalConfirmed); 
+      		    lineCaseData.push(lineCaseRow);
+			
+		    var lineDeathRow =[];
+       		    lineDeathRow.push(rowDate);
+      		    lineDeathRow.push(totalDeaths<0?0:totalDeaths);
+      		    lineDeathData.push(lineDeathRow);
+		    
+		    if (tableDay == rowDate) {   //table
+			var tableRow = [];
+			tableRow.push(totalConfirmed<0?0:totalConfirmed);
+			tableRow.push(totalDeaths<0?0:totalDeaths);
+			tableRow.push(newConfirmed<0?0:newConfirmed);
+			tableRow.push(newDeaths<0?0:newDeaths);
+			tableData.push(tableRow);
+		    }
+		});
+	    }
+
+	    var dataTable = google.visualization.arrayToDataTable(tableData);
+		
+	    var tableChart = new google.visualization.Table(document.getElementById('tableContainer'));	//table
+	    tableChart.draw(dataTable, {
+		    showRowNumber: true, 
+		    title: "Covid case status in " + selectedCounty + " County as of yesterday"});
+		
+	    var caseData = google.visualization.arrayToDataTable(caseChartData);
+	    var chart = new google.visualization.ColumnChart(document.getElementById("caseContainer"));
+	    chart.draw(caseData, {
+		width: 1600,
+		height: 800,
+		title: "Last 25 Days of Covid Confirmed Cases in " +  selectedCounty + " County"
+	    });
+	
+	    var deathData = google.visualization.arrayToDataTable(deathChartData);
+	    var chart1 = new google.visualization.ColumnChart(document.getElementById("deathContainer")); 
+	    chart1.draw(deathData, {
+		width: 1600,
+		height: 800,
+		colors: ['#a52714'],
+		title: "Last 25 Days of Covid Confirmed Deaths in " +  selectedCounty + " County"
+	    });	
+		
+	    var lineCaseData1 = google.visualization.arrayToDataTable(lineCaseData);
+ 	    var lineCaseChart = new google.visualization.LineChart(document.getElementById("lineChartTotalCase"));
+    	    lineCaseChart.draw(lineCaseData1, {width: 1600, height: 800, title: "Cumulative Covid-19 Cases in " +  selectedCounty + " County"});
+	
+	    var lineDeathData1 = google.visualization.arrayToDataTable(lineDeathData);
+ 	    var lineDeathChart = new google.visualization.LineChart(document.getElementById("lineChartTotalDeath"));
+    	    lineDeathChart.draw(lineDeathData1, {width: 1600, height: 800, colors: ['#a52714'], title: "Cumulative Covid-19 Caused Deaths in " +  selectedCounty + " County"}); 
+	}
---
+++ covid-maps.js
@@ -0,0 +1,51 @@
+	var map;
+	var xmlhttp = new XMLHttpRequest();
+	
+	function getCountyCases(county_name) {
+	    let case_info = "";
+	    xmlhttp.open("GET", "https://data.ca.gov/api/3/action/datastore_search?resource_id=926fd08f-cc91-4828-af38-bd45de97f8c3&q=" + getPastDate(1), false);
+	    xmlhttp.send();
+	    if (xmlhttp.status === 200) {
+		var myObj = JSON.parse(xmlhttp.responseText);
+		for (var i = 0; i < myObj.result.records.length; i++) {
+		    if (myObj.result.records[i].county == county_name) {
+			case_info = myObj.result.records[i].totalcountconfirmed;
+		    }
+		}
+	    }
+	    return case_info;
+	}
+
+	function initMap() {
+
+	    map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 5,
+		center: {
+		    lat: 43.733,
+		    lng: -122.53
+		}
+	    });
+
+	    map.data.loadGeoJson('https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/california-counties.geojson');
+
+	    map.data.setStyle({
+		fillColor: 'blue',
+		strokeWeight: 1
+	    });
+
+	    var infowindow = new google.maps.InfoWindow();
+
+	    map.data.addListener('click', function(event) {
+		let county_name = event.feature.getProperty("name");
+		selectedCounty = county_name;
+		drawChart();
+		let cases = getCountyCases(county_name)
+		let html = county_name + " County " + "New Cases: " + cases;
+		infowindow.setContent(html); // show the html variable in the infowindow
+		infowindow.setPosition(event.latLng); // anchor the infowindow at the marker
+		infowindow.setOptions({
+		    pixelOffset: new google.maps.Size(0, -30)
+		}); 
+		infowindow.open(map);
+	    });
+	}	

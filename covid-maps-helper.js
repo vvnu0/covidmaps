f6080c33b9c8ad043dd76be3607ce9eb8c4741da
nairvishnumail <76607707+nairvishnumail@users.noreply.github.com>
2020-12-31 18:07:49 -0800

    Adding second page for states in US

---
+++ covid-maps-helper.js
@@ -0,0 +1,106 @@
+function getFullStateName(stateTwoLetter) {
+		if (stateTwoLetter == "AL") {
+			return "Alabama"
+
+		} else if (stateTwoLetter == "AK") {
+			return "Alaska"
+		} else if (stateTwoLetter == "AZ") {
+			return "Arizona"
+		} else if (stateTwoLetter == "AR") {
+			return "Arkansas"
+		} else if (stateTwoLetter == "CA") {
+			return "California"
+		} else if (stateTwoLetter == "CO") {
+			return "Colorado"
+		} else if (stateTwoLetter == "CT") {
+			return "Connecticut"
+		} else if (stateTwoLetter == "DE") {
+			return "Delaware"
+		} else if (stateTwoLetter == "FL") {
+			return "Florida"
+		} else if (stateTwoLetter == "GA") {
+			return "Georgia"
+		} else if (stateTwoLetter == "HI") {
+			return "Hawaii"
+		} else if (stateTwoLetter == "ID") {
+			return "Idaho"
+		} else if (stateTwoLetter == "IL") {
+			return "Illinois"
+		} else if (stateTwoLetter == "IN") {
+			return "Indiana"
+		} else if (stateTwoLetter == "IA") {
+			return "Iowa"
+		} else if (stateTwoLetter == "KS") {
+			return "Kansas"
+		} else if (stateTwoLetter == "KY") {
+			return "Kentucky"
+		} else if (stateTwoLetter == "LA") {
+			return "Louisiana"
+		} else if (stateTwoLetter == "ME") {
+			return "Maine"
+		} else if (stateTwoLetter == "MD") {
+			return "Maryland"
+		} else if (stateTwoLetter == "MA") {
+			return "Massachusetts"
+		} else if (stateTwoLetter == "MI") {
+			return "Michigan"
+		} else if (stateTwoLetter == "MN") {
+			return "Minnesota"
+		} else if (stateTwoLetter == "MS") {
+			return "Mississippi"
+		} else if (stateTwoLetter == "MO") {
+			return "Missouri"
+		} else if (stateTwoLetter == "MT") {
+			return "Montana"
+		} else if (stateTwoLetter == "NE") {
+			return "Nebraska"
+		} else if (stateTwoLetter == "NV") {
+			return "Nevada"
+		} else if (stateTwoLetter == "NH") {
+			return "New Hampshire"
+		} else if (stateTwoLetter == "NJ") {
+			return "New Jersey"
+		} else if (stateTwoLetter == "NM") {
+			return "New Mexico"
+		} else if (stateTwoLetter == "NY") {
+			return "New York"
+		} else if (stateTwoLetter == "NC") {
+			return "North Carolina"
+		} else if (stateTwoLetter == "ND") {
+			return "North Dakota"
+		} else if (stateTwoLetter == "OH") {
+			return "Ohio"
+		} else if (stateTwoLetter == "OK") {
+			return "Oklahoma"
+		} else if (stateTwoLetter == "OR") {
+			return "Oregon"
+		} else if (stateTwoLetter == "PA") {
+			return "Pennsylvania"
+		} else if (stateTwoLetter == "RI") {
+			return "Rhode Island"
+		} else if (stateTwoLetter == "SC") {
+			return "South Carolina"
+		} else if (stateTwoLetter == "SD") {
+			return "South Dakota"
+		} else if (stateTwoLetter == "TN") {
+			return "Tennessee"
+		} else if (stateTwoLetter == "TX") {
+			return "Texas"
+		} else if (stateTwoLetter == "UT") {
+			return "Utah"
+		} else if (stateTwoLetter == "VT") {
+			return "Vermont"
+		} else if (stateTwoLetter == "VA") {
+			return "Virginia"
+		} else if (stateTwoLetter == "WA") {
+			return "Washington"
+		} else if (stateTwoLetter == "WV") {
+			return "West Virginia"
+		} else if (stateTwoLetter == "WI") {
+			return "Wisconsin"
+		} else if (stateTwoLetter == "WY") {
+			return "Wyoming"
+		} 
+                return "";
+
+        }
---
+++ covid-maps-states.js
@@ -0,0 +1,168 @@
+	let selectedState = "CA";
+
+        function initStateMap() {
+        }
+
+        function callMe() {
+           selectedState = document.getElementById("stateid").value;
+           drawChart();
+        }
+
+	function getPastDate(days) {
+	    currentDate = new Date();
+	    currentDate.setDate(currentDate.getDate() - days);
+	    const offset = currentDate.getTimezoneOffset();
+	    currentDate = new Date(currentDate.getTime() - (offset*60*1000));
+	    return currentDate.toISOString().split('T')[0];
+	}
+	
+
+	function getChartData() {
+	    var xmlhttp = new XMLHttpRequest();
+	    var query = "https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=" + getPastDate(1) + "T00:00:00.000"
+
+	    xmlhttp.open("GET", query, false);
+	    xmlhttp.send();
+	    return JSON.parse(xmlhttp.responseText);
+	}
+	
+	function drawChart() {
+	    var stateJsonData= getChartData();
+	    var stateCaseData = [];
+	    var stateDeathData = [];
+	    var stateTotalCaseData = [];
+	    var stateTotalDeathData = [];
+	    var stateTableData = [];
+		
+	    var date = getPastDate(1);
+	    	
+	    if (stateJsonData.length > 0) {
+		var stateTableColHead = ['Date', 'State', 'Total Confirmed Cases', 'Total Deaths', 'New Confirmed Cases', 'New Deaths'];
+		stateTableData.push(stateTableColHead );
+		    
+		var stateCaseColHead = ['State', 'New Confirmed'];
+		stateCaseData.push(stateCaseColHead); 
+		 
+		var stateDeathColHead = ['State', 'New Deaths'];
+		stateDeathData.push(stateDeathColHead);
+		    
+		var stateTotalCaseColHead = ['State', 'Total Confirmed'];
+   		stateTotalCaseData.push(stateTotalCaseColHead);
+		   
+		var stateTotalDeathColHead = ['State', 'Total Deaths'];
+   		stateTotalDeathData.push(stateTotalDeathColHead);
+	    
+		stateJsonData.forEach(function(row) {
+                    var stateName = getFullStateName(row['state']);
+                    if (row['state'].length == 2) {
+                       
+		    var newCase = parseInt(row['new_case']);
+		    var deaths = parseInt(row['new_death']);
+		    var totalCase = parseInt(row['tot_cases']);
+		    var totalNumDeaths = parseInt(row['tot_death']);
+
+	   
+		    var caseLine = []; 
+		    caseLine.push(stateName);
+		    caseLine.push(newCase<0?0:newCase);
+		    stateCaseData.push(caseLine);
+		    
+		    var deathLine = [];
+		    deathLine.push(stateName);
+		    deathLine.push(deaths<0?0:deaths);
+		    stateDeathData.push(deathLine);
+			
+		    var totalCaseLine =[];
+       		    totalCaseLine.push(stateName);
+       		    totalCaseLine.push(totalCase<0?0:totalCase); 
+      		    stateTotalCaseData.push(totalCaseLine);
+			
+		    var lineDeathRow =[];
+       		    lineDeathRow.push(stateName);
+      		    lineDeathRow.push(totalNumDeaths<0?0:totalNumDeaths);
+      		    stateTotalDeathData.push(lineDeathRow);
+		    
+		    if (selectedState == row['state']) {
+			var tableRow = [];
+			tableRow.push(date)
+			tableRow.push(stateName)
+			tableRow.push(totalCase<0?0:totalCase);
+			tableRow.push(totalNumDeaths<0?0:totalNumDeaths);
+			tableRow.push(newCase<0?0:newCase);
+			tableRow.push(deaths<0?0:deaths);
+			stateTableData.push(tableRow);
+		     }
+                  }
+		});
+
+
+	    var table = google.visualization.arrayToDataTable(stateTableData);
+	    var tableData = new google.visualization.Table(document.getElementById('stateTable'));	
+	    tableData.draw(table, {
+		    showRowNumber: true,
+	    }); 
+		
+	    var case_data = google.visualization.arrayToDataTable(stateCaseData);
+	    var caseChart = new google.visualization.ColumnChart(document.getElementById("stateCase"));
+	    caseChart.draw(case_data, {
+		width: 1600,
+		height: 800,
+		title: "State by State Comparison of New Cases",
+                hAxis:{
+                        title: 'States',
+                        gridlines: { count: 50 },
+			showTextEvery:1,
+			slantedText:true,
+			slantedTextAngle:90,
+			},
+	    });
+	
+	    var deathData = google.visualization.arrayToDataTable(stateDeathData);
+	    var deathChart = new google.visualization.ColumnChart(document.getElementById("stateDeath")); 
+	    deathChart.draw(deathData, {
+		width: 1600,
+		height: 800,
+		colors: ['#a52714'],
+		title: "State by State Comparison of New Cases",
+                hAxis:{
+                        title: 'States',
+                        gridlines: { count: 50 },
+			showTextEvery:1,
+			slantedText:true,
+			slantedTextAngle:90,
+			},
+	    });	
+		
+	    var totalCaseData = google.visualization.arrayToDataTable(stateTotalCaseData);
+ 	    var totalCaseChart = new google.visualization.ColumnChart(document.getElementById("stateTotalCase"));
+    	    totalCaseChart.draw(totalCaseData, {
+		width: 1600,
+		height: 800,
+		title: "State by State Comparison of Total Cases",
+                hAxis:{
+                        title: 'States',
+                        gridlines: { count: 50 },
+			showTextEvery:1,
+			slantedText:true,
+			slantedTextAngle:90,
+			},
+	    });
+	
+	    var totalDeathData = google.visualization.arrayToDataTable(stateTotalDeathData);
+ 	    var totalDeathChart = new google.visualization.ColumnChart(document.getElementById("stateTotalDeath"));
+    	    totalDeathChart.draw(totalDeathData, {
+		width: 1600,
+		height: 800,
+		colors: ['#a52714'],
+		title: "State by State Comparison of Total Cases",
+                hAxis:{
+                        title: 'States',
+                        gridlines: { count: 50 },
+			showTextEvery:1,
+			slantedText:true,
+			slantedTextAngle:90,
+			},
+	    });	 
+	}
+      }
+      
